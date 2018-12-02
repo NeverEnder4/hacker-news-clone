90fa2f5d234a34ed943ef18237f5f22293f8f446
NeverEnder4 <apettigrew.wsdev@gmail.com>
2018-12-02 13:49:33 -0800

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import fetch from 'isomorphic-fetch';
+
+export default class Index extends React.Component {
+  static getInitialProps() {}
+  render() {
+    return <div>Hacker Next</div>;
+  }
+}
